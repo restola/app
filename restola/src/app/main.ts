f115f6d1c3b9204603469592f541cca71ef846cd
MOHAMMED <Amine.MOHAMMED@saint-gobain.com>
2017-05-02 21:11:12 +0200

    init ionic

---
+++ restola/src/app/main.ts
@@ -0,0 +1,5 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { RestolaModule } from './app.module';
+
+platformBrowserDynamic().bootstrapModule(RestolaModule);
